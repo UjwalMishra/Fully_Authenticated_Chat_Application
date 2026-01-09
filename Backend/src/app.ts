9c54313279fa73e7569a93290f7dbcbd0b4c6ec4
75way-MBAir-05 <75way-mbair-05@75way-MBAir-05s-MacBook-Air.local>
2026-01-09 15:29:18 +0530

    feat(chat): implement room creation, joining, and room listing APIs

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes";
+import chatRoutes from "./modules/chat/chat.routes"
 
 export const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/chat",chatRoutes)
